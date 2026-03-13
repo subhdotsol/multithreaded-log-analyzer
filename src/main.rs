82b8b653e51e659209b7852c63bc0ff40bb6dcec
subh <subhajitchaudhury05@gmail.com>
2026-03-13 10:21:26 +0530

    feat : loads into the memory and splits into lines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,28 @@
+use std::fs;
+
 mod log_generator;
 
 use log_generator::log_generator;
 
 fn main() {
     log_generator().unwrap();
+    // getting the logs in memory
+    let logs = fs::read_to_string("logs.txt").expect("failed to read log files");
+    println!("Loaded {} bytes of logs", logs.len());
+
+    // at this point -> logs.txt ------> String in memory
+
+    let lines: Vec<&str> = logs.lines().collect();
+    println!("Loaded {} lines", lines.len());
+
+    // &str → "INFO Server started"
+    //   ├─ &str → "INFO User logged in"
+    //   ├─ &str → "ERROR Database connection failed"
+    //   └─ &str → "WARN Disk almost full"
+
+    // no new strings are being created each new line borrows from the original string
+    // this is called zero copy parsing
+
+    // copying every line to the memory would be expensive as my ex
+    println!("Total log lines: {}", lines.len());
 }
